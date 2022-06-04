9a46daec69df1aec3104bba2cad65fd9fc570cad
dongHun <actgong@gmail.com>
2022-06-04 14:23:51 +0900

    [Update] router.js 파일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,13 @@
-import { click } from '@testing-library/user-event/dist/click';
 import React, {useState, useEffect} from 'react';
-import './App.css';
+import {Link, RouteComponentProps} from 'react-router-dom';
 
-import Login from './components/LoginPage';
-import ProductInfo from './components/ProductInfo';
-import NavBar from './components/NavBar';
-import MainImage from './components/MainImage';
+import Main from './Main';
+import Router from './Router';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const [movieTitle, setMovieTitle] = useState('');
-  const [movieYear, setMovieYear] = useState('');
-
-  const [movies, setMovies] = useState(
-    [
-      {title: "별헤는밤", year: 2020},
-      {title: "귀곡성", year: 2019},
-      {title: "7번방의 기적", year: 2030},
-      {title: "범죄도시", year: 2034}
-    ]
-  );
-
-  // 아래와 같이 useEffect 함수를 이용하면, 원하는 변수의 state에 따라서만
-  // useEffect의 인자로 받은 함수를 실행한다.
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-  console.log('rendering');
 
   return (
-    <div className="App">
-      <NavBar imageLink="./images/glass.png"/>
-      {/*<form onSubmit={addMovie}>
-        <input
-          type="text"
-          value={movieTitle}
-          placeholder="영화 제목"
-          onChange={e => setMovieTitle(e.target.value)}
-        /> <br />
-        <input
-          type="text"
-          value={movieYear}
-          placeholder="개봉 년도"
-          onChange={e => setMovieYear(e.target.value)}
-        /> <br />
-        <button>영화 추가</button>
-      </form>
-      */}
-      <MainImage />
-
-      <div className="productList">
-        <ProductInfo imageLink="./images/logo192.png" productName="samsung" date="2022/04/05" />
-        <ProductInfo imageLink="./images/logo192.png" productName="apple" date="2001/03/01" /> 
-        <ProductInfo imageLink="./images/logo192.png" productName="korea" date="2021/03/01" />
-        <ProductInfo imageLink="./images/logo192.png" productName="korea" date="2021/03/01" />       
-      </div>
-    </div>
+    <Router />
   );
 }
 
